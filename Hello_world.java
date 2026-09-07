b28afb754debb9bfebdbc7019e9a79153e52e8af
Tanya Sharma <tanyasharma09050@gmail.com>
2026-09-08 02:07:30 +0530

    Added Hello World and test programs

---
+++ Hello_world.java
@@ -0,0 +1,11 @@
+class Hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
+class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
