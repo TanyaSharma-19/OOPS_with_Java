c8e794602c9673102639f2f340977bb27a55ac8c
Tanya Sharma <tanyasharma09050@gmail.com>
2026-09-11 12:07:17 +0530

    Add ShapeTest implementation

---
+++ Src/Use_case_mse1/ShapeTest.java
@@ -0,0 +1,49 @@
+import static java.lang.Math.*;
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[2];
+        shapes[0] = new Circle(5.0);
+        shapes[1] = new Rectangle(4.0, 6.0);
+ 
+        for (Shape s : shapes) {
+            s.displayArea();   // must print correct area for each shape
+        }
+    }
+}
+
+
+abstract class Shape{
+    abstract double area();
+
+    void displayArea(){
+        System.out.println("Area: " + area());
+    }
+    }
+
+class Circle extends Shape{
+    double radius;
+
+    Circle(double radius){
+        this.radius = radius;
+    }
+
+    double area(){
+        return PI* radius * radius;
+    }
+
+}
+
+class Rectangle extends Shape{
+
+    double length;
+    double width;
+
+    Rectangle(double length , double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    double area(){
+        return length * width;
+    }
+}
