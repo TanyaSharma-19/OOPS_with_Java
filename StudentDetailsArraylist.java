acc7819181c3d24e552df984fc5180b9c4fe8782
TanyaSharma-19 <tanyasharma09050@gmail.com>
2026-09-18 14:37:44 +0530

    Add files via upload

---
+++ StudentDetailsArraylist.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Student {
+
+    String name;
+    int rollNo;
+    int tmarks;
+
+    Student(String name, int rollNo, int tmarks) throws InvalidMarksException{
+
+        if(tmarks<0) throw new InvalidMarksException("Marks cannot be negative");
+        this.tmarks=tmarks;
+        this.rollNo=rollNo;
+        this.name=name;
+    }
+
+    void display(){
+        System.out.println("Student name is : "+name+" Student Roll no. is : "+rollNo+" Student marks : "+tmarks);
+    }
+    
+}
+
+public class StudentDetailsArraylist{
+    public static void main(String[] args) {
+        List<Student> li = new LinkedList<>();
+        try{
+        li.add(new Student("abc", 101, 90));
+        li.add(new Student("pqr", 102, 85));
+        li.add(new Student("xyz", 103, 95));
+        }
+
+        catch(InvalidMarksException e){
+            System.out.println(e);
+        }
+
+        for(Student i : li){
+            i.display();
+        }
+    }
+}
+
+class InvalidMarksException extends Exception{
+
+    InvalidMarksException(String msg){
+        super(msg);
+    }
+}
+
